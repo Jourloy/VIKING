8308e475d2157376a9720c4454d2cde0716ec241
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-02 02:55:01 +0300

    Added new function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
+/*
+ *
+ * ____________________ Screeps AI ____________________
+ * VIKING repository: https://github.com/Jourloy/VIKING
+ * Author: JOURLOY
+ *
+ *
+ */
+
 module.exports.loop = function () {
+    RoomStats();
+
+    for (i in Game.rooms) {
+        if (!Memory.rooms[`.${Game.rooms[i].name}.CalculateState`]) Calculate(Game.rooms[i])
+    }
+
     if (!Memory.Friends) Memory.Friends = [];
     else Memory.Friends = Memory.Friends;
 };
