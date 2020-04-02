baaba67ba0385679236d22b8c5ec4e2c4922bc76
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-02 23:19:07 +0300

    Added new function

---
+++ src/Memory/SetMemory.js
@@ -0,0 +1,10 @@
+// SetMemory start here
+
+function Friends() {
+    if (!Memory.Friends) Memory.Friends = [];
+    else Memory.Friends = Memory.Friends;
+}
+
+function SetMemory() {
+    let Friends();
+}
