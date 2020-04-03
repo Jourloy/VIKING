946c950cb75403ddb1964328f1a539cfce2e5faa
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-03 22:56:50 +0300

    Добавлена роль

---
+++ src/Creeps/Roles/Piece/harvester.js
@@ -0,0 +1,17 @@
+const Harvester = {
+    /** @param {Creep} creep **/
+    control(creep) {
+        if (creep.spawning) {
+            creep.memory.room = creep.room.name;
+        } else {
+            if (creep.room.name == creep.memory.room) {
+                const source = GetSource(creep);
+
+                if (creep.memory.mode === 0) harvest();
+                else (creep.memory.mode === 1) harvesterWork();
+            } else {
+                creep.moveTo(new RoomPosition(25, 25, creep.memory.room), { ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50 });
+            }
+        }
+    }
+}
