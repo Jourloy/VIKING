cfd68006b89334205133545a80c7413d1d2ebc68
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-03 22:56:35 +0300

    Создан класс

--- src/Console/Logging.js
+++ src/Console/Logging.js
@@ -1,29 +1,24 @@
 // Logging start here
 
-/**
-* @param {string} message
-*/
-function logging(message) {
-    console.log(`<p style="color: grey">[LOG] ${message}</p>`)
-}
+class logging {
+    /**
+    * @param {string} message
+    */
+    static info(message) {
+        console.log(`<p style="color: grey">[LOG] ${message}</p>`)
+    }
 
-/**
-* @param {string} message
-*/
-function warning(message) {
-    console.log(`<p style="color: orange">[WARNING] ${message}</p>`)
-}
+    /**
+    * @param {string} message
+    */
+    static warning(message) {
+        console.log(`<p style="color: orange">[WARNING] ${message}</p>`)
+    }
 
-/**
-* @param {string} message
-*/
-function error(message) {
-    console.log(`<p style="color: red">[ERROR] ${message}</p>`)
-}
-
-/**
-* @param {string} message
-*/
-function info(message) {
-    console.log(`<p style="color: white">[INFO] ${message}</p>`)
+    /**
+    * @param {string} message
+    */
+    static error(message) {
+        console.log(`<p style="color: red">[ERROR] ${message}</p>`)
+    }
 }
