ab6a4ff925c3fe096e4d695b6e9f7765d7497a29
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-03 22:56:25 +0300

    Переименован

--- src/console/GlobalFunctions.js
+++
@@ -1,6 +0,0 @@
-// Commands start here
-
-function AddPlayerInFriend(username) {
-    if (!Memory.Friends.includes(username)) Memory.Friends.push(username);
-    else console.log(`Oops. ${username} already in your friend list.`)
-}
