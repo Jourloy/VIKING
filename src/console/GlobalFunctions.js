6f43cf6c6b9d42bca8bc514bf7d0cf25288fd748
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-03 20:56:56 +0300

    Fixed

--- src/console/GlobalFunctions.js
+++ src/console/GlobalFunctions.js
@@ -1,6 +1,6 @@
 // Commands start here
 
-function AddPlayerInFriend = function(username) {
+function AddPlayerInFriend(username) {
     if (!Memory.Friends.includes(username)) Memory.Friends.push(username);
     else console.log(`Oops. ${username} already in your friend list.`)
 }
