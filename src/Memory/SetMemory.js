cec3b571bebf3181af31d8da1a432dd2a0a64db0
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-05 14:16:05 +0300

    Добавлен класс SetMemory

--- src/Memory/SetMemory.js
+++ src/Memory/SetMemory.js
@@ -1,37 +1,39 @@
 // SetMemory start here
 
-function BasicFolders() {
-    if (!Memory.Information) Memory.Information = {};
-    else Memory.Information = Memory.Information;
-
-    if (!Memory.RoomsState) Memory.RoomsState = {};
-    else Memory.RoomsState = Memory.RoomsState;
-
-    if (!Memory.SpawnFlags) Memory.SpawnFlags = {};
-    else Memory.SpawnFlags = Memory.SpawnFlags;
-}
-
-function Friends() {
-    if (!Memory.Friends) Memory.Friends = [];
-    else Memory.Friends = Memory.Friends;
-}
-
-/**
- * @param {string} param1 Name of room
- * @param {string} param2 Name of flag
- * @param {string} part
- */
-function AddInMemory(param1, param2, part) {
-    if (part == 'SpawnFlags') {
-        if (!Memory.SpawnFlags[param1]) {
-            Memory.SpawnFlags[param1] = {flagName:param2}
-        }
+class SetMemory {
+    static run() {
+        Friends();
+        BasicFolders()
     }
 
-    logging(`In [${part}] added [${param1}] and [${param2}]`);
-}
+    private BasicFolders() {
+        if (!Memory.Information) Memory.Information = {};
+        else Memory.Information = Memory.Information;
 
-function SetMemory() {
-    Friends();
-    BasicFolders()
+        if (!Memory.RoomsState) Memory.RoomsState = {};
+        else Memory.RoomsState = Memory.RoomsState;
+
+        if (!Memory.SpawnFlags) Memory.SpawnFlags = {};
+        else Memory.SpawnFlags = Memory.SpawnFlags;
+    }
+
+    private Friends() {
+        if (!Memory.Friends) Memory.Friends = [];
+        else Memory.Friends = Memory.Friends;
+    }
+
+    /**
+     * @param {string} param1 Name of room
+     * @param {string} param2 Name of flag
+     * @param {string} part
+     */
+    static AddInMemory(param1, param2, part) {
+        if (part == 'SpawnFlags') {
+            if (!Memory.SpawnFlags[param1]) {
+                Memory.SpawnFlags[param1] = {flagName:param2}
+            }
+        }
+
+        logging(`In [${part}] added [${param1}] and [${param2}]`);
+    }
 }
