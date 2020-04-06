dc4ad540966fb3fa4103e743ee1e8609f0fd4a61
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-06 20:20:17 +0300

    Начат автобилдер

---
+++ src/Room/Autobuilder.js
@@ -0,0 +1,16 @@
+function RCL2RoomBuilder(room) {
+    // const extensions
+    // const roads
+    // TODO: 
+}
+
+/**
+ * @param {Object} room
+ */
+function Autobuild(room) {
+    switch(room.controller.level) {
+        switch 2:
+            if (!room.memory.build) RCL2RoomBuilder(room);
+            break;
+    }
+}
