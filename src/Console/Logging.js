4e44511154b876de4c50796ddf0a3ed74f167d23
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-06 12:57:13 +0300

    Убран класс

--- src/Console/Logging.js
+++ src/Console/Logging.js
@@ -1,24 +1,22 @@
 // Logging start here
 
-class logging {
-    /**
-    * @param {string} message
-    */
-    static info(message) {
-        console.log(`<p style="color: grey">[LOG] ${message}</p>`)
-    }
+/**
+* @param {string} message
+*/
+function logging(message) {
+    console.log(`<p style="color: grey">[LOG] ${message}</p>`)
+}
 
-    /**
-    * @param {string} message
-    */
-    static warning(message) {
-        console.log(`<p style="color: orange">[WARNING] ${message}</p>`)
-    }
+/**
+* @param {string} message
+*/
+function warning(message) {
+    console.log(`<p style="color: orange">[WARNING] ${message}</p>`)
+}
 
-    /**
-    * @param {string} message
-    */
-    static error(message) {
-        console.log(`<p style="color: red">[ERROR] ${message}</p>`)
-    }
+/**
+* @param {string} message
+*/
+function error(message) {
+    console.log(`<p style="color: red">[ERROR] ${message}</p>`)
 }
