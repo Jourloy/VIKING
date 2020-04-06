b76e7ae86f89c0ff1bd5c0050cd8c416279b1096
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-06 12:57:35 +0300

    Добавлена функция получения тела крипа из паттерна

--- src/main.js
+++ src/main.js
@@ -32,53 +32,137 @@ function GenerateString(length) {
 }
 
 /**
- * @param {Object} spawn
- * @param {list} pattern
- * @param {int} count
- * @param {bool} notBalance
- * @return {list}
+ *
+ *  ------------------------------------------------------------------------------
+ * | This code was given by Sergey on Screeps Slack. Thank you very much :)       |
+ *  ------------------------------------------------------------------------------
+ * | Code corrected |
+ *  ----------------
+ *
+ * @param {BODYPART} body
+ * @return {int} Priority of this part
  */
-
-function getBodyParts(spawn, pattern, count, notBalance) {
-
-    if (!spawn) return 'Need parameter [Spawn]';
-    if (!pattern) return 'Need parameter [Pattern]';
-    if (!count || count == null) count = 25;
-    if (!notBalance) notBalance = false;
-
-    let body = [];
-    let prepareBody = [];
-    let amountEnergy = spawn.room.energyCapacityAvailable;
-    let bodyCost = 0;
-
-    count = count * 2;
-
-    if (!notBalance) {
-        for (let i = 0; i < count/2; i++) {
-            prepareBody.push(pattern[i%pattern.length]);
-            prepareBody.push(MOVE);
-        }
+function bodyPriority(body) {
+    switch (body) {
+        case HEAL:
+            return -1;
+            break;
+        case MOVE:
+            return 2;
+            break;
+        case RANGED_ATTACK:
+            return 1;
+            break;
+        case ATTACK:
+            return 0;
+            break;
+        case WORK:
+            return 7;
+            break;
+        case TOUGH:
+            return 10;
+            break;
+        default:
+            return 5;
+            break;
     }
-
-    // TODO: Дописать getBodyParts
-
-    for (i in prepareBody) {
-        if (prepareBody[i] != MOVE && bodyCost + BODYPART_COST[prepareBody[i]] + 50 < amountEnergy) {
-            body.push(prepareBody[i]);
-            body.push(MOVE)
-            bodyCost += BODYPART_COST[prepareBody[i]] + 50;
-        }
-    }
-    return body
 }
 
-function spawnCreep() {
+/**
+ *
+ *  ------------------------------------------------------------------------------
+ * | This code was given by Sergey on Screeps Slack. Thank you very much :)       |
+ *  ------------------------------------------------------------------------------
+ * | Code corrected |
+ *  ----------------
+ *
+ * @param {Object} room
+ * @param {List} pattern
+ * @param {int} count
+ * @param {Object} optional
+ */
+ function getBodyParts(room, pattern, count, optional) {
+     const roads = optional.isForRoad || false; // Move per 1 body part or move per 2 body parts
+     let moveBoost = optional.moveBoost || null; // Will be used boosts?
+     const priority = {}; // Unused
+     const skipCarry = optional.skipCarry || false; // Skip carry parts?
+     const mustBe = optional.mustBe || []; // Important body parts. For example result will be [ATTACK, CARRY, CARRY, MOVE, MOVE] with pattern [CARRY]
+     if (moveBoost &&
+         !_.includes([RESOURCE_ZYNTHIUM_OXIDE, RESOURCE_ZYNTHIUM_ALKALIDE, RESOURCE_CATALYZED_ZYNTHIUM_ALKALIDE], moveBoost)) {
+         logger.logWarning(MODULE_NAME, `Incorect parameter moveBoost: ${moveBoost}`);
+         moveBoost = null;
+     }
+     let step = 1;
+     if (roads) {
+         step = 2;
+     }
+     step *= (optional && optional.moveEach) || 1;
+     if (moveBoost) {
+         step +=
+             BOOSTS.move[moveBoost].fatigue - 1;
+     }
+     let index = 0;
+     let moveIndex = 0;
+     let availableEnergy = room.energyAvailable;
+     if (optional && optional.maxEnergy) {
+         availableEnergy = Math.min(optional.maxEnergy, availableEnergy);
+     }
+     let body = [];
+     mustBe.forEach(b => {
+         if ((!optional || !optional.withoutMove) && (b != CARRY || !skipCarry)) {
+             if (moveIndex == 0) {
+                 availableEnergy -= BODYPART_COST[MOVE];
+                 if (availableEnergy < BODYPART_COST[b]) {
+                     return false;
+                 }
+                 body.push(MOVE);
+             }
+             moveIndex = (moveIndex + 1) % step;
+         }
+         availableEnergy -= BODYPART_COST[b];
+         if (availableEnergy < 0) {
+             return false;
+         }
+         body.push(b);
+         return;
+     });
+     while (body.length < 50 && count > 0) {
+         if ((!optional || !optional.withoutMove) && (pattern[index] != CARRY || !skipCarry)) {
+             if (moveIndex == 0) {
+                 availableEnergy -= BODYPART_COST[MOVE];
+                 if (availableEnergy < BODYPART_COST[pattern[index]]) {
+                     break;
+                 }
+                 body.push(MOVE);
+             }
+             moveIndex = (moveIndex + 1) % step;
+         }
+         availableEnergy -= BODYPART_COST[pattern[index]];
+         if (availableEnergy < 0) {
+             break;
+         }
+         body.push(pattern[index]);
+         count--;
+         index = (index + 1) % pattern.length;
+     }
+     return body.sort((a, b) => (priority[b] || bodyPriority(b)) - (priority[a] || bodyPriority(a)));
+ }
 
+function spawnCreep() {
+    for (i in Game.rooms) {
+        let room = Game.rooms[i];
+
+        let opt = {
+            isForRoad: true,
+        }
+        console.log(getBodyParts(room, [WORK, CARRY], 12, opt))
+    }
 }
 
 module.exports.loop = function () {
     SetMemory();
     RoomStats();
+    spawnCreep();
 
     for(i in Game.rooms) {
         if (!Memory.RoomsState[Game.rooms[i].name]) CalculateRoom(Game.rooms[i].name);
