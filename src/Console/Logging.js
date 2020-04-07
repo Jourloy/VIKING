80155949a4c968a2252bc5f019f7f60dd6b55518
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-07 20:17:09 +0300

    Добавлено описание

--- src/Console/Logging.js
+++ src/Console/Logging.js
@@ -1,22 +1,28 @@
 // Logging start here
 
 /**
-* @param {string} message
-*/
+ * Print in console message with [LOG] mark with grey color
+ *
+ * @param {string} message
+ */
 function logging(message) {
     console.log(`<p style="color: grey">[LOG] ${message}</p>`)
 }
 
 /**
-* @param {string} message
-*/
+ * Print in console message with [WARNING] mark with orange color
+ *
+ * @param {string} message
+ */
 function warning(message) {
     console.log(`<p style="color: orange">[WARNING] ${message}</p>`)
 }
 
 /**
-* @param {string} message
-*/
+ * Print in console message with [ERROR] mark with red color
+ *
+ * @param {string} message
+ */
 function error(message) {
     console.log(`<p style="color: red">[ERROR] ${message}</p>`)
 }
