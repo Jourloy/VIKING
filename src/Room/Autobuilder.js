a975f947948a387b6e07e7215a32cafd78ab3211
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-12 21:26:35 +0300

    Обновлено

--- src/Room/Autobuilder.js
+++ src/Room/Autobuilder.js
@@ -10,20 +10,24 @@ function CheckStructures(room, basicStructures) {
         if (spawns.length < basicStructures.spawns) spawnsState = false;
         else spawnsState = true;
     } else spawnsState = true;
+
     if (basicStructures.extensions) {
         const extensions = room.find(FIND_MY_STRUCTURES, {filter: s => s.structureType == STRUCTURE_EXTENSION});
         if (extensions.length < basicStructures.extensions) extensionsState = false;
         else extensionsState = true;
     } else extensionsState = true;
+
     if (basicStructures.towers) {
         const towers = room.find(FIND_MY_STRUCTURES, {filter: s => s.structureType == STRUCTURE_TOWER});
         if (towers.length < towers.extensions) towersState = false;
         else towersState = true;
     } else towersState = true;
+
     if (basicStructures.storage) {
         if (!room.storage) storageState = false;
         else storageState = true;
     } else storageState = true;
+    
     if (basicStructures.terminal) {
         if (!room.terminal) terminalState = false;
         else terminalState = true;
