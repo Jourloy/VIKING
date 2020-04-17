7d8661fba079efb270c594e2750928e1f5e0a98c
Igor Shaposhnikov <jourloy@yandex.ru>
2020-04-17 23:12:14 +0300

    Добавлена информация о ресурсах для работы с терминалом

---
+++ src/Room/Terminal.js
@@ -0,0 +1,34 @@
+// Terminal start here
+
+const RESOURCE_INFO = {
+    Energy:{
+        CloseOrders:{
+            Buy:{
+
+            },
+            Sell:{
+                Amount: 1000,
+                Price: 0.04,
+                Cost: 1000
+            }
+        },
+        OpenOrders:{
+
+        }
+    },
+    OXYGEN:{
+        CloseOrders:{
+            Buy:{
+
+            },
+            Sell:{
+                Amount: 1000,
+                Price: 0.02,
+                Cost: 1000,
+            }
+        },
+        OpenOrders:{
+            
+        }
+    }
+}
