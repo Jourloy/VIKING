fa4b0446519fcf511945029a966e8c6254794450
Igor Shaposhnikov <jourloy@yandex.ru>
2020-11-13 02:32:57 +0300

    Add example of creep

---
+++ src/Creeps/example.js
@@ -0,0 +1,15 @@
+// Example.js
+
+const options = {
+    name: 'EXAMPLE',
+    target: 'example',
+    move: {
+        heuristicWeight: 1.4
+    }
+}
+
+const creep = new Creep(options);
+
+creep.run = function() {
+    // Creep logic
+}
