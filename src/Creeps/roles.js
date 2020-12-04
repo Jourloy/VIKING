c40203d1cea47e6938b7aa0064ef00a070e07e04
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-05 00:11:56 +0300

    v0.1.43

--- src/Creeps/roles.js
+++ src/Creeps/roles.js
@@ -1,45 +1,4 @@
-// Roles.js
-
-const miner = new VikingCreep({
-    role: 'miner',
-    body: {
-        pattern: [WORK],
-        count: 5,
-        mustBe: [CARRY],
-        moveParts: false,
-    }
-});
-miner.run = (creep) => {
-    creepSay(creep, this.role);
-}
-
-const worker = new VikingCreep({
-    role: 'worker',
-    body: {
-        patter: [WORK, CARRY],
-    }
-});
-worker.run = (creep) => {
-    if (creep.memory.birthRoom == null) creep.memory.birthRoom = creep.room.name;
-    else if (creep.room.name === creep.memory.birthRoom) {
-        if (creep.memory.train == null || creep.memory.train.state === false) {
-            if (creep.store.getUsedCapacity() === 0) creep.memory.mode = 0;
-            else if (creep.store.getUsedCapacity() === creep.store.getCapacity()) creep.memory.mode = 1;
-
-            if (creep.memory.mode === 0) GetResource(creep, RESOURCE_ENERGY);
-            else if (creep.memory.mode === 1) HarvesterWork(creep);
-        } else {
-            const target = Game.getObjectById(creep.memory.train.target);
-            if (target != null) {
-                if (!Game.getObjectById(target.memory.destinationId).structureType || Game.getObjectById(target.memory.destinationId).structureType !== 'container') {
-                    if (target.pos.isNearTo(Game.getObjectById(target.memory.destinationId))) creep.memory.train.role = '-';
-                } else {
-                    if (target.pos.isEqualTo(Game.getObjectById(target.memory.destinationId))) creep.memory.train.role = '-';
-                }
-            } else creep.memory.train.role = '-';
-        }
-    } else creep.moveTo(new RoomPosition(25, 25, creep.memory.birthRoom), moveParams);
-}
+// roles.js
 
 function runCreeps() {
     for (let i in Game.creeps)
