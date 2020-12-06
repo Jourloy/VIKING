ffb62ba1bf0373ca6db00f2c15792226b8c46710
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-07 01:32:28 +0300

    v0.1.136

--- src/Creeps/roles/worker.js
+++ src/Creeps/roles/worker.js
@@ -1,9 +1,25 @@
 // worker.js
 
+function workerLogic(creep) {
+
+    const cs = creep.room.find(FIND_CONSTRUCTION_SITES);
+    let structures = creep.room.find(FIND_MY_STRUCTURES, {
+        filter: (strc) => {
+            return strc.store && (strc.store.getCapacity() == null ? strc.store.getUsedCapacity(RESOURCE_ENERGY) < strc.store.getCapacity(RESOURCE_ENERGY) : strc.store.getUsedCapacity() < strc.store.getCapacity());
+        }
+    });
+
+    structures = structures.sort(function (a, b) { return spawn.pos.getRangeTo(a) - spawn.pos.getRangeTo(b); });
+
+    if (cs.length > 0) creep.buildJR();
+    if (structures.length > 0) creep.refillJR()
+    else creep.upgradeJR();
+}
+
 const worker = new VikingCreep({
     role: 'worker',
     body: {
-        patter: [WORK, CARRY],
+        pattern: [WORK, CARRY],
     }
 });
 worker.run = (creep) => {
@@ -12,9 +28,9 @@ worker.run = (creep) => {
         if (creep.memory.train == null || creep.memory.train.state === false) {
             if (creep.store.getUsedCapacity() === 0) creep.memory.mode = 0;
             else if (creep.store.getUsedCapacity() === creep.store.getCapacity()) creep.memory.mode = 1;
-
-            if (creep.memory.mode === 0) GetResource(creep, RESOURCE_ENERGY);
-            else if (creep.memory.mode === 1) HarvesterWork(creep);
+            console.log(creep.getResource(creep))
+            if (creep.memory.mode === 0) creep.getResource();
+            else if (creep.memory.mode === 1) workerLogic(creep);
         } else {
             const target = Game.getObjectById(creep.memory.train.target);
             if (target != null) {
