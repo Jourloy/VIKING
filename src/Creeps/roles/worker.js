87e5a82fd9df367a8087dce132f19b726a3bad0b
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-09 19:45:28 +0300

    v0.1.243

--- src/Creeps/roles/worker.js
+++ src/Creeps/roles/worker.js
@@ -8,11 +8,12 @@ function workerLogic(creep) {
             return strc.store && (strc.store.getCapacity() == null ? strc.store.getUsedCapacity(RESOURCE_ENERGY) < strc.store.getCapacity(RESOURCE_ENERGY) : strc.store.getUsedCapacity() < strc.store.getCapacity());
         }
     });
-
+    const spawns = creep.room.find(FIND_MY_SPAWNS);
+    const spawn = spawns[0];
     structures = structures.sort(function (a, b) { return spawn.pos.getRangeTo(a) - spawn.pos.getRangeTo(b); });
 
-    if (cs.length > 0) creep.buildJR();
     if (structures.length > 0) creep.refillJR()
+    else if (cs.length > 0) creep.buildJR(cs[0]);
     else creep.upgradeJR();
 }
 
@@ -25,20 +26,23 @@ const worker = new VikingCreep({
 worker.run = (creep) => {
     if (creep.memory.birthRoom == null) creep.memory.birthRoom = creep.room.name;
     else if (creep.room.name === creep.memory.birthRoom) {
-        if (creep.memory.train == null || creep.memory.train.state === false) {
+
+        if (creep.memory.state == null) {
             if (creep.store.getUsedCapacity() === 0) creep.memory.mode = 0;
             else if (creep.store.getUsedCapacity() === creep.store.getCapacity()) creep.memory.mode = 1;
             if (creep.memory.mode === 0) creep.getResource();
             else if (creep.memory.mode === 1) workerLogic(creep);
-        } else {
+        } else if (creep.memory.state === 'trainHead') {
             const target = Game.getObjectById(creep.memory.train.target);
             if (target != null) {
                 if (!Game.getObjectById(target.memory.destinationId).structureType || Game.getObjectById(target.memory.destinationId).structureType !== 'container') {
-                    if (target.pos.isNearTo(Game.getObjectById(target.memory.destinationId))) creep.memory.train.role = '-';
+                    if (target.pos.isNearTo(Game.getObjectById(target.memory.destinationId))) creep.memory.state = null;
                 } else {
-                    if (target.pos.isEqualTo(Game.getObjectById(target.memory.destinationId))) creep.memory.train.role = '-';
+                    if (target.pos.isEqualTo(Game.getObjectById(target.memory.destinationId))) creep.memory.state = null;
                 }
-            } else creep.memory.train.role = '-';
-        }
+            } else creep.memory.state = null;
+        } else if (creep.memory.state === 'busy') {
+            creep.say('🛑 BUSY')
+        } else creep.memory.state = null;
     } else creep.moveTo(new RoomPosition(25, 25, creep.memory.birthRoom), moveParams);
 }
