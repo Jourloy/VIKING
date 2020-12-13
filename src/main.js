c03569873572fac56153c3c9f690d43c9ee23c24
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-13 21:52:37 +0300

    v0.2.213

--- src/main.js
+++ src/main.js
@@ -1,36 +1,12 @@
 // Main.js
 
-/* module.exports.loop = function() {
-    if (_screeps.public() && Game.cpu.bucket > 5000) Game.cpu.generatePixel();
-
-    Tower.run();
-    _room.run();
-
-    for (i in Memory.creeps) {
-        if (!Game.creeps[i]) delete Memory.creeps[i]
-    }
-
-    spawnCreeps()
-    runCreeps()
-    for (i in roomsArray) {
-        const room = Game.rooms[roomsArray[i].name];
-        Autobuild(room)
-    }
-}; */
-
 function main() {
+    if (_screeps.public() && Game.cpu.bucket === 10000 && _viking.redFlags() === false) Game.cpu.generatePixel();
+
+    _memory.run();
     _room.run();
     _creeps.run();
     _tower.run();
-
-    for (i in Memory.creeps) {
-        if (!Game.creeps[i]) delete Memory.creeps[i]
-    }
-
-    for (i in roomsArray) {
-        const room = Game.rooms[roomsArray[i].name];
-        Autobuild(room)
-    }
 }
 
 exports.loop = main();
