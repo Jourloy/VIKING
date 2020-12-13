ef8a336bb45a53faf0668dc5f63d660f14e234ca
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-13 21:52:57 +0300

    v0.2.213

--- src/Rooms/autobuilder.js
+++ src/Rooms/autobuilder.js
@@ -94,7 +94,7 @@ function BuildInRoom(room) {
     //for (i in RCL2['extension']['pos']) {
     //    room.createConstructionSite(RCL2['extension']['pos'][i].x, RCL2['extension']['pos'][i].y, STRUCTURE_EXTENSION)
     //}
-    if (room.controller.level > 3) for (i in containers) room.createConstructionSite(containers[i], STRUCTURE_CONTAINER);
+    if (room.controller.level >= 2) for (i in containers) room.createConstructionSite(containers[i], STRUCTURE_CONTAINER);
 }
 
 function CheckStructures(room, basicStructures) {
