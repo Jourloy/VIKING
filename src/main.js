ba15e442c3d5d5b5f430a070bfa2ea9dd69d5e84
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-20 18:00:43 +0300

    v0.4.189

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ function main() {
     if (_screeps.public() && Game.cpu.bucket === 10000 && _viking.redFlags() === false) Game.cpu.generatePixel();
 
     _memory.run();
-    _room.run();
+    _rooms.run();
     _creeps.run();
     _tower.run();
     _sandbox.run();
