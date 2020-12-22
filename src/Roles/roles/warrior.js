3d0838b1b0eb07c861d882de1fe5d8808fabbbd6
Igor Shaposhnikov <jourloy@yandex.ru>
2020-12-22 19:14:22 +0300

    v0.4.218

--- src/Roles/roles/warrior.js
+++ src/Roles/roles/warrior.js
@@ -8,7 +8,35 @@ const warrior = new _creep({
 });
 warrior.run = (creep) => {
     if (creep.spawning) return;
-    if (Game.flags.fastAttack) {
+    if (creep.memory.destination == null) {
+        if (creep.room.defenseFlag.length > 0) {
+            const hostiles = creep.room.hostileCreeps.length;
+            let myCreeps = creep.room.find(FIND_MY_CREEPS);
+            myCreeps = myCreeps.filter(aCreep => (aCreep.memory.role === 'warrior' && aCreep.memory.destination === 'defense'));
+
+            if (myCreeps < Math.ceil(hostiles / 2)) creep.memory.destination = 'defense';
+        }
+
+        if (Game.flags.fastAttack) creep.memory.destination = 'fastAttack'
+    }
+
+    if (creep.memory.destination === 'defense') {
+        let targetCreeps = undefined;
+
+        console.log(creep.room.invaders)
+        if (creep.room.invaders.length > 0) targetCreeps = creep.room.invaders;
+
+        if (targetCreeps != null) {
+            if (creep.room.controller && creep.room.controller.my && creep.room.controller.safeMode != null) creep._say('ODIN WITH US', true);
+            else creep._say('FOR THE VIKINGS', true)
+            if (creep.attack(targetCreeps[0]) === ERR_NOT_IN_RANGE) creep.travelTo(targetCreeps[0]);
+        } else {
+            const spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+            if (spawn.recycleCreep(creep) === ERR_NOT_IN_RANGE) creep.travelTo(spawn);
+        }
+    }
+
+    if (creep.memory.destination === 'fastAttack') {
         if (Game.flags.fastAttack.room == null || creep.room !== Game.flags.fastAttack.room) creep.travelTo(Game.flags.fastAttack);
         else {
             const hostileCreeps = creep.findHostileCreeps();
